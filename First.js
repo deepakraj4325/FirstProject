633cfb7fcc4cebf028d0b5da9e9373088da55c3b
deepakraj4325 <119610338+deepakraj4325@users.noreply.github.com>
2024-07-03 01:34:57 +0530

    Update First.js

--- First.js
+++ First.js
@@ -1,1 +1,12 @@
 'use strict';
+function lipsync() {
+let gameStarted=false,
+
+gameUpdate =()=>{};
+ let faceDistance = {};
+ let reqPredMouthCoord=[];
+ let meshArr=[];
+const Max_Continuious_Check=5;
+
+}
+
